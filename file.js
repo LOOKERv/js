bc517f262fb69038a0bda3837d080390c89f0dc7
looker n a <gera.nikitin1996@gmail.com>
2025-03-31 17:55:42 +0300

    first commit

---
+++ file.js
@@ -0,0 +1,15 @@
+"use strict"; //болеее сторогий режим js нельзя создавать переменные без const / let и тд
+
+//alert("!!!!!!!!!!а!привет!!!!");
+// let loool = 5;
+// let lol,
+//   str = "hi";
+// console.log(loool, lol, str);
+// lol = prompt("WW, num ");
+// console.log(lol);
+let value = 5;
+value = 15;
+const value1 = 10;
+debugger; // для отладки остановится здесь
+console.log(value);
+console.log(typeof value); // узнать тип данных внутри переменной

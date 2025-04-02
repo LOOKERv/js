184623006b1634c929febca8f4870020cb0862ce
looker n a <gera.nikitin1996@gmail.com>
2025-04-02 17:57:10 +0300

    3.2

--- file.js
+++ file.js
@@ -1,16 +1,22 @@
 "use strict"; //болеее сторогий режим js нельзя создавать переменные без const / let и тд
 
-//alert("!!!!!!!!!!а!привет!!!!");
-// let loool = 5;
-// let lol,
-//   str = "hi";
-// console.log(loool, lol, str);
-// lol = prompt("WW, num ");
-// console.log(lol);
-let value = 5;
-value = 15;
-const value1 = 10;
-debugger; // для отладки остановится здесь
-console.log(value);
-console.log(typeof value); // узнать тип данных внутри переменной
-https://stepik.org/lesson/1407780/step/1?unit=1425243
+// //alert("!!!!!!!!!!а!привет!!!!");
+// // let loool = 5;
+// // let lol,
+// //   str = "hi";
+// // console.log(loool, lol, str);
+// // lol = prompt("WW, num ");
+// // console.log(lol);
+// let value = 5;
+// value = 15;
+// const value1 = 10;
+// debugger; // для отладки остановится здесь
+// console.log(value);
+// console.log(typeof value); // узнать тип данных внутри переменной
+//
+let num = 10.5;
+console.log(Math.ceil(num));
+console.log(Math.round(num));
+console.log(Math.trunc(num));
+console.log(Math.floor(num));
+//https://stepik.org/lesson/1407794/step/3?unit=1425258
